238374c0718e02f75d2c9aa59f687ac82ca58f8d
silby <silamakankamissoko@gmail.com>
2025-09-25 09:42:18 +0200

    Add an environment var PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import loginRoute from './routes/loginRoute.js';
 import appointementRoute from './routes/appointementRoute.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,8 @@
-import { PrismaClient } from "@prisma/client";
 import express from "express";
 
 const router = express.Router();
-const prisma = new PrismaClient();
 
 router.get('/', (req, res) => {
-  const users = prisma.user.findMany();
   res.json({ message: 'Welcome to the Doctolib API'});
 });
 
